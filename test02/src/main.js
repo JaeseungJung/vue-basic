45f372c2b49eeb58a207b19af4ad252497486c5b
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-07-11 23:05:18 +0900

    UPDATE

--- test02/src/main.js
+++ test02/src/main.js
@@ -3,20 +3,25 @@ import App from "./App.vue";
 import router from "./router";
 import ApiMixin from "./api";
 import store from "./store/store";
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+//Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin);
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+
 
 createApp(App).use(router).use(store).mixin(ApiMixin).mount("#app");
 
 window.Kakao.init("8d23aa9d01e67a499183ec489d2ba43d");
 window.Kakao.isInitialized();
 
-import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 
-// Make BootstrapVue available throughout your project
-// Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-// Vue.use(IconsPlugin);
+
--- test02/src/views/Databinding.vue
+++ test02/src/views/Databinding.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Hello {{ title1 }}</h1>
+    
   </div>
 
   <div>
